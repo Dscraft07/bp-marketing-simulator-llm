bac568d523a33253fc922c7cfa73215e1da502bb
David Sambazov <david.sambazov@emplifi.io>
2025-10-09 12:22:27 +0200

    Creating pages for embedding forms

---
+++ app/(protected)/campaigns/new/page.tsx
@@ -0,0 +1,9 @@
+import { CampaignForm } from "@/app/campaigns/components/CampaignForm";
+
+export default function NewCampaignPage() {
+  return (
+    <div className="flex justify-center">
+      <CampaignForm />
+    </div>
+  );
+}
---
+++ app/(protected)/target-groups/new/page.tsx
@@ -0,0 +1,9 @@
+import { TargetGroupForm } from "@/app/target-groups/components/TargetGroupForm";
+
+export default function NewTargetGroupPage() {
+  return (
+    <div className="flex justify-center">
+      <TargetGroupForm />
+    </div>
+  );
+}
